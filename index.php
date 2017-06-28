82e5b133ebc4d6c819406219999ceec64bdb2986
Your Name <you@example.com>
2017-06-28 15:11:50 +0200

    Beginning

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="script/index.css">
+    <title>Accueil</title>
+</head>
+<body>
+<section id="container">
+    <header class="hidden">
+        <h2>Identification</h2>
+    </header>
+    <img id="bando" src="img/ifa-bando.png" alt="IFATECH">
+    <img id="pano" src="img/ifa-pano.jpg" alt="IFA dev">
+    <div id="log">
+        <form action="index.php">
+            <label for="pseudo" class="left">Pseudo</label><input type="text" name="pseudo" id="pseudo">
+            <label for="mdp" class="left">Mot de passe</label><input type="text" name="mdp" id="mdp">
+        </form>
+    </div>
+</section>
+</body>
+</html>
